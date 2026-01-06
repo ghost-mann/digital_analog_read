11d6e589267634394f4e5f0376d80f8748b19557
ghost-mann <iamaob8@protonmail.com>
2026-01-06 16:32:30 +0300

    feat: added analog_button.cpp and its functions and variable in button.h

--- src/button.h
+++ src/button.h
@@ -2,8 +2,12 @@
 #define BUTTON_H
 
 extern int buttonPin;
+extern int analog_buttonPin;
 
 void init_button();
 void monitor_button();
+
+void init_analog_button();
+void monitor_analog_button();
 
 #endif
